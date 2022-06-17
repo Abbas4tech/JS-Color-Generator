9cfd1924031b680fd050c1abe5bc5f879d533940
Abbas4tech <shaikhabbas54@gmail.com>
2022-06-18 02:26:19 +0530

    initial generator

---
+++ app.js
@@ -0,0 +1,18 @@
+const body = document.getElementById("root");
+const displayText = document.getElementById("color-text");
+const pallete = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0, "a", "b", "c", "d", "e", "f"];
+displayText.innerHTML = body.style.backgroundColor;
+const colorGenerator = (arr, mode = 6) => {
+  const color = arr
+    .map((e) => ({ e, sortkey: Math.random() }))
+    .sort((a, b) => a.sortkey - b.sortkey)
+    .map(({ e }) => e)
+    .splice(0, mode)
+    .join("");
+
+  body.style.backgroundColor = `#${color}`;
+  displayText.innerHTML = `#${color}`;
+};
+document
+  .getElementById("genBtn")
+  .addEventListener("click", () => colorGenerator(pallete));
